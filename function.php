a05d830a90a8c3a72f3c69f93fd23bc7220bcdf3
mihaelZemlyanskiy <73349068+mihaelZemlyanskiy@users.noreply.github.com>
2021-12-10 18:04:34 +0300

    Update function.php

--- function.php
+++ function.php
@@ -5,7 +5,29 @@
 	require_once 'connect.php';
 				$arrNews = mysqli_query($connect,"SELECT * FROM  `testnews`");
 				$arrNews = mysqli_fetch_all($arrNews);							/*Массив со всеми данными*/
-	
+
+function showNews($arrNews,$idst){						/*шаблон статей*/
+
+					$arrTime=explode('-',$arrNews[$idst-1][4]);
+					$timeStampDate=mktime(0,0,0,$arrTime[1],$arrTime[2],$arrTime[0]);
+
+	echo "<h2>" . $arrNews[$idst-1][1] . "</h2>
+			
+			<div class='blockstone'>
+			<img src='/img/".$arrNews[$idst-1][3]."'>
+				<div>
+					<p class='time'>".date('d.m.Y',$timeStampDate)."</p>
+					<p class='text'>".$arrNews[$idst-1][2]."</p>
+				</div>
+			</div>
+			<div class='blocksttwo'>
+				".$arrNews[$idst-1][5]."
+			</div>
+			<div class='newsmore'><a href='/news/'>Еще новости</a></div>
+	";
+
+}
+
 	function AllNews($arrNews){													/*Вывод всех новостей на страничку*/
 				foreach($arrNews as $arrValues){
 
@@ -14,8 +36,8 @@
 					
 
 					echo "
-					<a href=''>
-						<img src=img/".$arrValues[3].">
+					<a href='".$arrValues[0]."'>
+						<img src='/img/".$arrValues[3]."'>
 							<div><p class='title'>".$arrValues[1]."</p>
 								<p class='time'>".date('d.m.Y',$timeStampDate)."</p>
 								<p class='text'>".$arrValues[2]."</p>
@@ -24,7 +46,7 @@
 
 				}
 	}
-	function LastNews($arrNews){												/*Вывод последних новостей на страничку*/
+function LastNews($arrNews){												/*Вывод последних новостей на страничку*/
 
 
 		$newsone=0;		/*ключ к первой новости*/
@@ -34,10 +56,7 @@
 		$idone=0;		
 		$idtwo=0;
 
-		$reslut=0;		/*массив для трех последних новостей*/
-		
 	
-
 		foreach($arrNews as $arrValues){
 			$arrTime=explode('-',$arrValues[4]);
 			$timeStampDate=mktime(0,0,0,$arrTime[1],$arrTime[2],$arrTime[0]);
@@ -74,23 +93,18 @@
 		$result[1]=$arrtwo;
 		$result[2]=$arrthree;
 		
-		for($i=0;$i<count($result);$i++){
+	for($i=0;$i<count($result);$i++){
 					echo "
-<a href=''>
-	<img src=img/".$result[$i][3].">
-	<div><p class='title'>".$result[$i][1]."</p>
-		<p class='time'>".$time[$i]."</p>
-		<p class='text'>".$result[$i][2]."</p>
-	</div>
-</a>";
+	<a href='/news/".$result[$i][0]."'>
+		<img src='img/".$result[$i][3]."'>
+		<div><p class='title'>".$result[$i][1]."</p>
+			<p class='time'>".$time[$i]."</p>
+			<p class='text'>".$result[$i][2]."</p>
+		</div>
+	</a>";
 
 
 
-		}
-	}	
-
-	
-
-
-
+	}
+}	
 ?>
